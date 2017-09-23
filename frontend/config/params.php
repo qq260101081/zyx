3745803dc64489cbe711aeaf5827f21b5774c4d5
carl <260101081@qq.com>
2017-09-23 16:10:53 +0800

    1

---
+++ frontend/config/params.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'WECHAT' => [
+        'app_id'  => 'wxb5a6a475020c9b22',         // AppID
+        'secret'  => '037816e40b8fdc69313553d9124a6778',     // AppSecret
+        'token'   => 'yangyan',          // Token
+        'aes_key' => '',                    // EncodingAESKey，安全模式下请一定要填写！！！
+    ]
+];
